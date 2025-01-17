4493e2bde36a55f9c347710a3ae751b7dc099852
jaylin <jaylinhernandez123@icloud.com>
2025-01-16 22:22:08 -0500

    I was able to figure out the text boxes, how to retrieve the text, and store it

--- javascript.js
+++ javascript.js
@@ -1,18 +1,47 @@
-const libary = []
+const libary = [
+  
+  
+  {Author:"John fed",Title:"Diary of a Wimpy", Pages:420, Read:"yes"},
+  {Author:"Perry Wood", Title:"Hunting Bird", Pages:30, Read:"Yes"},
+  {Author:"Luffy", Title:"One piece", Pages:1000,Read:"Yes"}
+]
 
 
 
-function Book(){
-
-
-
+function addBookToLibary(book){
+  libary.push(book)
+   for(var index in libary){
+    console.log(libary[index]["Author"])
+   }
+    
 
 }
+a=0
 
+function book(){
+    const inputAuthor = document.getElementById("author");
+    const author=inputAuthor.value;
+    document.getElementById("author").value=""
 
-function addbooktolibary(){
+    const inputTitle = document.getElementById("title")
+    const title= inputTitle.value
+  document.getElementById("title").value=""
 
+    const inputPages = document.getElementById("pages")
+    const pages = inputPages.value;
+  document.getElementById("pages").value=""
 
+    const inputRead = document.getElementById("read")
+    const read = inputRead.value
+  document.getElementById("read").value=""
 
-    
+  const book = {
+    Author: author,
+    Title : title,
+    Pages: pages,
+    Read: read
+  }
+  addBookToLibary(book)
+ 
+
 }
