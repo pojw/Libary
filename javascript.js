65cacd7c04226786731fbfb41fd90d6215a8f16d
jaylin <jaylinhernandez123@icloud.com>
2025-01-18 01:02:05 -0500

    I coded the javascript bascis to add the new books

--- javascript.js
+++ javascript.js
@@ -1,22 +1,93 @@
 const libary = [
   
   
-  {Author:"John fed",Title:"Diary of a Wimpy", Pages:420, Read:"yes"},
-  {Author:"Perry Wood", Title:"Hunting Bird", Pages:30, Read:"Yes"},
-  {Author:"Luffy", Title:"One piece", Pages:1000,Read:"Yes"}
+  {Author:"John Fed",Title:"Diary of a Wimpy Kid", Pages:420, Read:"on"},
+  {Author:"Perry Wood", Title:"Hunting Bird", Pages:30, Read:"off"},
+  {Author:"Luffy", Title:"One Piece", Pages:1000,Read:"on"}
 ]
 
 
+// libary[index]["Author"]
 
 function addBookToLibary(book){
   libary.push(book)
+  const book_reset = document.getElementById("books")
+  book_reset.innerHTML = ""
    for(var index in libary){
-    console.log(libary[index]["Author"])
+    const container = document.getElementById("books")
+
+    const card = document.createElement('div')
+    card.classList.add("card")
+
+// Author Section
+    const author_section = document.createElement("div")
+    author_section.classList.add("description")
+
+    const author_title = document.createElement("div")
+    author_title.textContent="Author"
+    author_section.appendChild(author_title)
+
+    const author_name = document.createElement("div")
+    author_name.textContent = libary[index]["Author"]
+    author_section.appendChild(author_name)
+
+// Title section
+    const title_section = document.createElement("div")
+    title_section.classList.add("description")
+
+    const title_name = document.createElement("div")
+    title_name.textContent="Title"
+    title_section.appendChild(title_name)
+
+    const title_realname = document.createElement("div")
+    title_realname.textContent= libary[index]["Title"]
+    title_section.appendChild(title_realname)
+
+// Page section
+
+    const pages = document.createElement("div")
+    pages.classList.add("description")
+
+    const pages_name =document.createElement("div")
+    pages_name.textContent="Pages"
+    pages.appendChild(pages_name)
+
+    const page_real = document.createElement("div")
+    page_real.textContent= libary[index]["Pages"]
+    pages.appendChild(page_real)
+
+// Read section 
+    const read = document.createElement("div")
+    read.classList.add("description")
+
+    const read_text = document.createElement("div")
+    read_text.textContent="Read"
+    read.appendChild(read_text)
+
+    const read_real = document.createElement("input")
+    read_real.type="checkbox"
+    if(libary[index]["Read"]=="on"){
+      read_real.checked=true
+    }
+    else{
+      read_real.checked=false
+    }
+  
+    read.appendChild(read_real)
+
+
+
+// adding to card container 
+    card.appendChild(title_section)
+    card.appendChild(author_section)
+    card.appendChild(pages)
+    card.appendChild(read)
+    container.appendChild(card)
    }
     
 
 }
-a=0
+
 
 function book(){
     const inputAuthor = document.getElementById("author");
@@ -33,7 +104,8 @@ function book(){
 
     const inputRead = document.getElementById("read")
     const read = inputRead.value
-  document.getElementById("read").value=""
+    console.log(inputRead.value)
+    inputRead.checked=false;
 
   const book = {
     Author: author,
